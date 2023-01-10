e746e7558ab0336051d04b423c88ccf3611a8737
ayequill <ayequillgh@gmail.com>
2023-01-10 23:54:16 +0000

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import { Navbar } from './components/Navbar'
-import { Hero } from './components/Hero'
-import Card from './components/Card';
-import './styles/scss/App.scss'
-import data from './data';
-
+import { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import Card from "./components/Card";
+import "./styles/scss/App.scss";
+import data from "./data";
 
 function App() {
   // iterate over the data to create props array
-  const cardElements = data.map((card)=>{
-    return <Card 
-    key = {card.id}
-    img = {card.coverImg} 
-    rating = {card.stats.rating}
-    reviewCount = {card.stats.reviewCount}
-    country = {card.location} 
-    title = {card.title}
-    price = {card.price}
-    openSpots = {card.openSpots} />
-  })
+  const cardElements = data.map((card) => {
+    return (
+      <Card
+        // always include a 'key'
+        key={card.id}
+        {...card}
+        // card = {card}
+      />
+    );
+  });
+
   return (
     <div className="container">
-     <Navbar />
-     <Hero />
-     <section className='sec-2'>
-
-  {cardElements}
-    </section>
+      <Navbar />
+      <Hero />
+      <section className="sec-2">{cardElements}</section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,32 @@
+export default function Card(props) {
+  console.log(props);
 
-
-export default function Card (props){
-    console.log(props.country)
-    
-    let badgeText
-    if (props.openSpots === 0){
-        badgeText = "SOLD OUT"
-    }else if(props.country === 'Online'){
-        badgeText = 'ONLINE'
-    }else badgeText = props.country
-    return(
-            <div className="card">
-                <div className='card-img'>
-                <img src={props.img} alt="" />
-                 <div className='card-status'>
-                    <p>{badgeText}</p>
-                </div>
-                </div>
-                <div className="card-details">
-                    <p className='rating'><i class="bi bi-star-fill"></i> {props.rating} <span>({props.reviewCount})⸰{props.country}</span></p>
-                    <p className='title'>{props.title}</p>
-                    <p className='price'><strong>From ${props.price}</strong> / person</p>
-                </div>
-            </div>
-    )
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  } else badgeText = props.location;
+  return (
+    <div className="card">
+      <div className="card-img">
+        <img src={props.coverImg} alt="" />
+        <div className="card-status">
+          <p>{badgeText}</p>
+        </div>
+      </div>
+      <div className="card-details">
+        <p className="rating">
+          <i class="bi bi-star-fill"></i> {props.stats.rating}{" "}
+          <span>
+            ({props.stats.reviewCount})⸰{props.location}
+          </span>
+        </p>
+        <p className="title">{props.title}</p>
+        <p className="price">
+          <strong>From ${props.price}</strong> / person
+        </p>
+      </div>
+    </div>
+  );
 }
